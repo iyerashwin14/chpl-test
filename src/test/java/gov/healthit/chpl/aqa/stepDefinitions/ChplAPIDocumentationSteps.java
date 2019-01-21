c19ca96e00b3106d29eb902b154faafc473be862
Ashwin Iyer <iyerashwin14@gmail.com>
2019-01-21 05:48:44 -0500

    feat: verify API endpoints documentation update

--- src/test/java/gov/healthit/chpl/aqa/stepDefinitions/ChplAPIDocumentationSteps.java
+++ src/test/java/gov/healthit/chpl/aqa/stepDefinitions/ChplAPIDocumentationSteps.java
@@ -2,15 +2,20 @@ package gov.healthit.chpl.aqa.stepDefinitions;
 
 import static org.testng.Assert.assertTrue;
 
+import java.io.FileReader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
 import gov.healthit.chpl.aqa.pageObjects.ChplAPIPage;
 
 /**
@@ -18,12 +23,13 @@ import gov.healthit.chpl.aqa.pageObjects.ChplAPIPage;
  */
 public class ChplAPIDocumentationSteps extends Base {
 
+    private JSONArray apiImplNoteList;
     /**
      * Constructor creates new driver.
      */
     public ChplAPIDocumentationSteps() {
         super();
-       }
+    }
 
     /**
      * Get user to the API Documentation page.
@@ -36,33 +42,43 @@ public class ChplAPIDocumentationSteps extends Base {
     }
 
     /**
-     * Click open Controller list.
-     * @param controller desired controller to open
+     * Assert that implementation notes for the endpoints link is updated.
+     * @throws Exception if File not found
      */
-    @When("^I click on \"([^\"]*)\" to see operations list$")
-    public void openController(final String controller) {
-        WebElement link = ChplAPIPage.controllerLink(getDriver(), controller);
-        ((JavascriptExecutor) getDriver()).executeScript("arguments[0].click();", link);
-    }
-
-    /**
-     * Click open given endpoint details.
-     * @param apiEndpoint given endpoint to view details
-     * @param endpointLink link to open details
-     */
-    @And("^I click on \"([^\"]*)\" using \"([^\"]*)\" link to view details$")
-    public void viewEndpointDetails(final String apiEndpoint, final String endpointLink) {
-        WebElement link = ChplAPIPage.endpointLink(getDriver(), endpointLink);
-        ((JavascriptExecutor) getDriver()).executeScript("arguments[0].click();", link);
-    }
-
-    /**
-     * Assert that text is updated.
-     * @param imptext expected text
-     */
-    @Then("^endpoint details should show updated text \"([^\"]*)\"$")
-    public void verifyUpdatedTextInCertifiedProductEndpointDetails(final String imptext) {
-        String impNotes = ChplAPIPage.certifiedProductsImplementationNotes(getDriver()).getText();
-        assertTrue(impNotes.contains(imptext), "Expect " + imptext + " to be found in " + impNotes);
+    @Then("^API endpoints should show updated implementation notes$")
+    public void apiEndpointsImplementationNotesDisplaysUpdatedText() throws Exception {
+        JSONParser jsonParser = new JSONParser();
+        Boolean failedCase = false;
+        Path resourceDirectory = Paths.get("src", "test", "resources", "implementationNotes.json");
+        FileReader reader = new FileReader(resourceDirectory.toString());
+        Object notesObj = jsonParser.parse(reader);
+        apiImplNoteList = (JSONArray) notesObj;
+        for (Object object : apiImplNoteList) {
+            failedCase = false;
+            JSONObject noteObject = (JSONObject) object;
+            JSONArray apiimplnotesArray  = (JSONArray) noteObject.get("apiimplnote");
+            for (Object endPointLink:apiimplnotesArray) {
+                JSONArray endPointLinkArrays = (JSONArray) ((JSONObject) endPointLink).get("endPointLink");
+                String controller = (String) ((JSONObject) endPointLink).get("controllerName");
+                WebElement cLink = ChplAPIPage.controllerLink(getDriver(), (String) ((JSONObject) endPointLink).get("controllerName"));
+                ((JavascriptExecutor) getDriver()).executeScript("arguments[0].click();", cLink);
+                for (Object links:endPointLinkArrays) {
+                    String endPointLink1 = (String) (((JSONObject) links).get("link"));
+                    WebElement epLink = ChplAPIPage.endpointLink(getDriver(), endPointLink1);
+                    ((JavascriptExecutor) getDriver()).executeScript("arguments[0].click();", epLink);
+                    String implNote = (String) ((JSONObject) links).get("implementationNote");
+                    String actImpNotes = ChplAPIPage.certifiedProductsImplementationNotes(getDriver()).getText();
+                    try {
+                        assertTrue(actImpNotes.contains(implNote), "Expected [ " + implNote + " ]not found for:[" + controller + "] where endpoint is:[" + endPointLink + "]");
+                    } catch (AssertionError ex) {
+                        System.out.println(ex.getMessage());
+                        failedCase = true;
+                    }
+                }
+            }
+            if (failedCase) {
+                throw new AssertionError("Scenario failed cases");
+            }
+        }
     }
 }
